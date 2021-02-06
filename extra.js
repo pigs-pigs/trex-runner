ad8d1e2dc8232d1a6ad42e7595d23fcece4eb1b5
Jason <63813682+pigs-pigs@users.noreply.github.com>
2021-02-06 12:57:23 -0500

    Update extra.js

--- extra.js
+++ extra.js
@@ -8,7 +8,7 @@ document.getElementById("botStatus").addEventListener("change", function() {
 		    var obstacles = Runner.instance_.horizon.obstacles;
 		    if (!tRex.jumping && (obstacles.length > 0) && (obstacles[0].xPos + obstacles[0].width) <= ((parseInt(Runner.instance_.currentSpeed - 0.1) - 5) * 34 + 160) && (obstacles[0].xPos + obstacles[0].width) > 20) {
 		        console.log(obstacles[0].xPos + obstacles[0].width + " | " + ((parseInt(Runner.instance_.currentSpeed - 0.1) - 5) * 34 + 160));
-		        tRex.startJump(tRex.currentSpeed);
+		        tRex.startJump();
 		    }
 		}, INTERVAL);
 	}
