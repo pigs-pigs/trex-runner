0f0a2b829f3e4cf66499b1af21d57c0b0ff0ec3c
Jason <63813682+pigs-pigs@users.noreply.github.com>
2021-02-24 13:34:39 -0500

    Update extra.js

--- extra.js
+++ extra.js
@@ -8,7 +8,7 @@ document.getElementById("botStatus").addEventListener("change", function() {
 		    var obstacles = Runner.instance_.horizon.obstacles;
 		    if (!tRex.jumping && (obstacles.length > 0) && (obstacles[0].xPos + obstacles[0].width) <= ((parseInt(Runner.instance_.currentSpeed - 0.1) - 5) * 34 + 160) && (obstacles[0].xPos + obstacles[0].width) > 20) {
 		        console.log(obstacles[0].xPos + obstacles[0].width + " | " + ((parseInt(Runner.instance_.currentSpeed - 0.1) - 5) * 34 + 160));
-			    if(obstacles[0].type == 'PTERODACTYL'){
+			    if(obstacles[0].typeConfig.type == 'PTERODACTYL'){
 				    tRex.setDuck(true)
 			    } else {
 				if (tRex.ducking){
